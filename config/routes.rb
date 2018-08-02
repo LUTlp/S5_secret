3399f2767b4e9c14253ec68502c3e780aaaa823d
LUTlp <lutlp@outlook.com>
2018-08-02 21:32:47 +0200

    repo avant final

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root   'static#home'
 
-  get    '/signup',  to: 'static#secret'
-  get   '/page-secrete', to: 'static#secret', as: 'pagesecrete'
+  get    '/signup',  to: 'users#new'
+  get   '/page-secrete',  to: 'static#secret', as: 'pagesecrete'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
